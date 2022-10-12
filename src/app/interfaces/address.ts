624ed840abda6dca3b5150c0a0983f5b32b63145
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-10-13 00:13:49 +0200

    test request address php

--- src/app/interfaces/address.ts
+++ src/app/interfaces/address.ts
@@ -2,9 +2,9 @@ import { User } from "./user";
 
 export interface Address {
     id?: number;
-    address_street?: string;
-    address_postalcode?: number;
-    address_city?: string;
-    address_country?: string;
+    street?: string;
+    postalcode?: number;
+    city?: string;
+    country?: string;
     users?: Array<User>;
 }
