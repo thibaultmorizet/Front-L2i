3a79f9d77b13e1cad3d95c3e3e88c743ef1e85b9
Thibaultmorizet <thibaultmorizet@people-in.fr>
2022-11-30 20:36:21 +0100

    fix/few bug after first return

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -3,27 +3,36 @@ import { Book } from '../interfaces/book';
 
 import { Injectable } from '@angular/core';
 import { Contact } from '../interfaces/contact';
+import { NgxIzitoastService } from 'ngx-izitoast';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContactService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private iziToast: NgxIzitoastService,
+    private translate: TranslateService
+  ) {}
 
   sendMail(contactForm: Contact) {
-      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-      this.http
-        .post(
-          'https://formspree.io/f/mkneqdqq',
-          {
-            name: contactForm.subject,
-            replyto: contactForm.email,
-            message: contactForm.message,
-          },
-          { headers: headers }
-        )
-        .subscribe((response) => {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    this.http
+      .post(
+        'https://formspree.io/f/mkneqdqq',
+        {
+          name: contactForm.subject,
+          replyto: contactForm.email,
+          message: contactForm.message,
+        },
+        { headers: headers }
+      )
+      .subscribe((response) => {
+        this.iziToast.success({
+          message: this.translate.instant('izitoast.mail_sent'),
+          position: 'topRight',
         });
-    
+      });
   }
 }
