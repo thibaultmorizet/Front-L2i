0742e0f6a10c2e3c1f91ad7d44dc78aa8eeda3de
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-01-08 20:11:47 +0100

    create homepage for moderator

--- src/app/components/moderator-management/moderator-home/moderator-home.component.spec.ts
+++ src/app/components/moderator-management/moderator-home/moderator-home.component.spec.ts
@@ -1,4 +1,19 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import {
+  FacebookLoginProvider,
+  SocialAuthServiceConfig,
+  SocialLoginModule,
+} from 'angularx-social-login';
+import { NgxIziToastModule } from 'ngx-izitoast';
+import { DialogModule } from 'primeng/dialog';
+import { TableModule } from 'primeng/table';
+import { HttpLoaderFactory } from 'src/app/app.module';
+import { AdminFooterComponent } from '../../partial/admin-footer/admin-footer.component';
+import { AdminHeaderComponent } from '../../partial/admin-header/admin-header.component';
 
 import { ModeratorHomeComponent } from './moderator-home.component';
 
@@ -8,9 +23,45 @@ describe('ModeratorHomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ModeratorHomeComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        ModeratorHomeComponent,
+        AdminHeaderComponent,
+        AdminFooterComponent,
+      ],
+      imports: [
+        NgxIziToastModule,
+        HttpClientModule,
+        RouterTestingModule,
+        SocialLoginModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient],
+          },
+        }),
+        FormsModule,
+        TableModule,
+        DialogModule,
+      ],
+      providers: [
+        {
+          provide: 'SocialAuthServiceConfig',
+          useValue: {
+            autoLogin: false,
+            providers: [
+              {
+                id: FacebookLoginProvider.PROVIDER_ID,
+                provider: new FacebookLoginProvider('496421065692400'),
+              },
+            ],
+            onError: (err) => {
+              console.error(err);
+            },
+          } as SocialAuthServiceConfig,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ModeratorHomeComponent);
     component = fixture.componentInstance;
--- src/app/components/moderator-management/moderator-home/moderator-home.component.ts
+++ src/app/components/moderator-management/moderator-home/moderator-home.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { NgxIzitoastService } from 'ngx-izitoast';
+import { User } from 'src/app/interfaces/user';
+import StorageCrypter from 'storage-crypter';
 
 @Component({
   selector: 'app-moderator-home',
   templateUrl: './moderator-home.component.html',
-  styleUrls: ['./moderator-home.component.css']
+  styleUrls: ['./moderator-home.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ModeratorHomeComponent implements OnInit {
+  storageCrypter = new StorageCrypter('Secret');
+  connectedAdmin: User | null = {};
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private iziToast: NgxIzitoastService,
+    private translate: TranslateService
+  ) {}
 
   ngOnInit(): void {
+    try {
+      this.connectedAdmin = JSON.parse(
+        this.storageCrypter.getItem('moderatorUser', 'session')
+      );
+    } catch (error) {
+      this.connectedAdmin = null;
+      this.router.navigateByUrl('/moderator/login');
+    }
+
+    if (this.storageCrypter.getItem('jeton', 'local')) {
+      if (this.tokenExpired(this.storageCrypter.getItem('jeton', 'local'))) {
+        this.moderatorLogout();
+      }
+    }
   }
 
+  tokenExpired(token: string) {
+    const expiry = JSON.parse(atob(token.split('.')[1])).exp;
+    return Math.floor(new Date().getTime() / 1000) >= expiry;
+  }
+
+  moderatorLogout() {
+    this.storageCrypter.removeItem('jeton', 'local');
+    this.storageCrypter.removeItem('cart', 'local');
+    this.storageCrypter.removeItem('user', 'session');
+    this.storageCrypter.removeItem('moderatorUser', 'session');
+    this.storageCrypter.removeItem('moderatorUser', 'session');
+    this.storageCrypter.removeItem('language', 'session');
+    this.connectedAdmin = null;
+    this.router.navigateByUrl('/moderator/login');
+    this.iziToast.success({
+      message: this.translate.instant('izitoast.you_re_logout'),
+      position: 'topRight',
+    });
+  }
+
+  
 }
