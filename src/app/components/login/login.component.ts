f8761ac059077600404148e33515a00ca1206335
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-03-01 15:20:43 +0100

    try to reconnect google sign in

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -124,6 +124,9 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    console.log(0, this.userLogin.email);
+    console.log(1, this.userLogin.password);
+
     this.us.getTheUser(this.userLogin.email).subscribe((theUser) => {
       if (theUser[0] == undefined) {
         this.errorEmail = 'We did not find an account with this email address';
@@ -141,68 +144,47 @@ export class LoginComponent implements OnInit {
         this.errorEmail = null;
         console.log(0, this.userLogin.email);
         console.log(1, this.userLogin.password);
-        if (this.userLogin.email == undefined) {
-          try {
-            this.userLogin.email = JSON.parse(
-              this.storageCrypter.getItem('googleLoginEmail', 'session')
-            );
-          } catch (error) {}
-        }
-        if (this.userLogin.password == undefined) {
-          try {
-            this.userLogin.password = JSON.parse(
-              this.storageCrypter.getItem('googleLoginPassword', 'session')
-            );
-          } catch (error) {}
-        }
-        setTimeout(() => {
-          console.log(2, this.userLogin);
+        console.log(2, this.userLogin);
 
-          this.as.login(this.userLogin).subscribe({
-            next: (res) => {
-              console.log(3, res);
+        this.as.login(this.userLogin).subscribe({
+          next: (res) => {
+            if (res.token != null) {
+              this.storageCrypter.setItem('jeton', res.token, 'local');
 
-              if (res.token != null) {
-                this.storageCrypter.setItem('jeton', res.token, 'local');
+              this.storageCrypter.setItem(
+                'user',
+                JSON.stringify(theUser[0]),
+                'session'
+              );
 
-                this.storageCrypter.setItem(
-                  'user',
-                  JSON.stringify(theUser[0]),
-                  'session'
+              this.connectedUser = theUser[0];
+              this.errorPassword = null;
+              try {
+                this.translate.setDefaultLang(
+                  this.connectedUser.language != undefined
+                    ? this.connectedUser.language
+                    : ''
                 );
-
-                this.connectedUser = theUser[0];
-                this.errorPassword = null;
-                try {
-                  this.translate.setDefaultLang(
-                    this.connectedUser.language != undefined
-                      ? this.connectedUser.language
-                      : ''
-                  );
-                } catch (error) {
-                  this.translate.setDefaultLang('en');
-                }
-                this.userLogin = {};
-                this.storageCrypter.removeItem('googleLoginEmail', 'session');
-                this.storageCrypter.removeItem(
-                  'googleLoginPassword',
-                  'session'
-                );
-                this.iziToast.success({
-                  message: this.translate.instant('izitoast.successful_login'),
-                  position: 'topRight',
-                });
-
-                setTimeout(() => {
-                  this.router.navigateByUrl('/home');
-                }, 250);
+              } catch (error) {
+                this.translate.setDefaultLang('en');
               }
-            },
-            error: (res) => {
-              this.errorPassword = 'Incorrect password';
-            },
-          });
-        }, 1000);
+              this.userLogin = {};
+              this.storageCrypter.removeItem('googleLoginEmail', 'session');
+              this.storageCrypter.removeItem('googleLoginPassword', 'session');
+              this.iziToast.success({
+                message: this.translate.instant('izitoast.successful_login'),
+                position: 'topRight',
+              });
+
+              setTimeout(() => {
+                this.router.navigateByUrl('/home');
+              }, 250);
+            }
+          },
+          error: (res) => {
+            this.errorPassword = 'Incorrect password';
+          },
+        });
       }
     });
     this.loginAfterRegister = false;
@@ -263,16 +245,9 @@ export class LoginComponent implements OnInit {
         this.userInscription.firstname = decode_token.given_name;
         this.userInscription.password = decode_token.sub;
         this.userInscription.token = decode_token.sub;
-        this.storageCrypter.setItem(
-          'googleLoginEmail',
-          decode_token.email,
-          'session'
-        );
-        this.storageCrypter.setItem(
-          'googleLoginPassword',
-          decode_token.sub,
-          'session'
-        );
+
+        this.userLogin.email = decode_token.email;
+        this.userLogin.password = decode_token.sub;
 
         this.register();
       }
