33db689a937b07bb882853bf3a62db15a7cf29a8
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-04-15 18:32:49 +0200

    add categories in global search

--- src/app/interfaces/video.ts
+++ src/app/interfaces/video.ts
@@ -1,6 +1,11 @@
 import { Brand } from './brand';
+import {Author} from "./author";
+import {Category} from "./category";
 
 export interface Video {
   id?: number;
   brand?: Brand;
+  author?: Array<Author>;
+  category?: Array<Category>;
+
 }
